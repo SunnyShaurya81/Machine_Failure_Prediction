ae26f1b03b2ca80224e426ef9a81f7d7472c7b5a
SunnyShaurya81 <itoilimpossible@gmail.com>
2026-02-26 17:25:02 +0000

    Add end-to-end MLOps pipeline with GitHub Actions

---
+++ model_building/deploy.py
@@ -0,0 +1,20 @@
+from huggingface_hub import HfApi
+import os
+
+HF_TOKEN = os.getenv("HF_TOKEN")
+SPACE_ID = "SunnyShaurya1981/engine-predictive-maintenance-app"
+
+api = HfApi(token=HF_TOKEN)
+
+files = ["app.py", "requirements.txt", "Dockerfile"]
+
+for file in files:
+    api.upload_file(
+        path_or_fileobj=file,
+        path_in_repo=file,
+        repo_id=SPACE_ID,
+        repo_type="space",
+        commit_message=f"Update {file}"
+    )
+
+print("Deployment successful!")
